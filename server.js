6b3470ecb4af7f2171d0a5f96bbc49900ffb7144
Tmeng <tonymengt@gmail.com>
2022-09-09 13:50:59 -0400

    added PORT

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
+const PORT = process.env.PORT || 3001
 const app = express();
 
 const { animals } = require('./data/animal')
 
-const port = 3001;
 
 filterByQuery = (query, animalsArray) => {
     let personalityTraitsArray = [];
@@ -54,7 +54,7 @@ app.get('/api/animals', (req, res) => {
     res.json(results);
 });
 
-app.listen(port, () => {
-    console.log('API server now on port 3001');
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}`);
 });
 
