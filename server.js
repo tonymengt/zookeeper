72b56787376ff75ea3314d238d08380b81632339
Tmeng <tonymengt@gmail.com>
2022-09-09 14:10:35 -0400

    added req.params and 404 redirect

--- server.js
+++ server.js
@@ -44,6 +44,11 @@ filterByQuery = (query, animalsArray) => {
     return filteredResults;
 };
 
+findById = (id, animalsArray) => {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
+
 
 app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -53,6 +58,15 @@ app.get('/api/animals', (req, res) => {
     console.log(results)
     res.json(results);
 });
+
+app.get('/api/animals/:id', (req,res) => {
+    const result = findById(req.params.id, animals);
+    if(result) {
+        res.json(result)
+    } else {
+        res.send(404)
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}`);
